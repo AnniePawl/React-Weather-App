8099d5f5bb3259c482a7c6f3d316ce025f0fb564
Anna Pawl <annampawl@gmail.com>
2019-04-15 16:30:47 -0700

    refactored Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,23 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 
 function Weather(props) {
-    const { main, description, icon } = props.weatherData.weather[0]
-    const { temp, pressure, humidity, temp_min, temp_max } = props.weatherData.main
-    return (
-        <div>
-            <div>Title: {main}</div>
-            <div>Desc: {description}</div>
-            <div>Icon: {icon}</div>
-            <div>Temp: {temp}</div>
-            <div>Pressure: {pressure}</div>
-            <div>Humidity: {humidity}</div>
-            <div>Temp Min: {temp_min} Max:{temp_max}</div>
-        </div>
-    )
+  // eslint-disable-next-line react/prop-types
+  const { weatherData } = props;
+  const { main, description, icon } = weatherData.weather[0];
+  const { temp, pressure, humidity, temp_min, temp_max } = weatherData.main;
+  return (
+    <div>
+      <div>
+        Title: {main}</div>
+      <div>Desc: {description}</div>
+      <div>Icon: {icon}</div>
+      <div>Temp: {temp}</div>
+      <div>Pressure: {pressure}</div>
+      <div>Humidity: {humidity}</div>
+      <div>Temp Min: {temp_min} Max:{temp_max}</div>
+    </div>
+  );
 }
 
-export default Weather
+export default Weather;
